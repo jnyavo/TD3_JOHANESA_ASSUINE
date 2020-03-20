633b3a62d080e645ae9320a5437d96789231577a
jesen-chrichne <jesenchrichne@gmail.com>
2020-03-20 21:11:46 +0400

    Correction du programme

--- addTwoINT.py
+++ addTwoINT.py
@@ -4,7 +4,19 @@ def add(a, b):
 def main():
 	import sys
 	c = sys.argv
+	if (len(c) != 3):
+		
+		print("Erreur : 2 argument seulement")
+		a = input("Entrer le 1er nombre : ")
+		b = input("Entrer le 2e nombre : ")
+		try:
+			print(c(a,b))
+		except:
+			print(c(eval(a),eval(b)))
+		return		
 	d = int(sys.argv[1])
 	e = int(sys.argv[2])
 	print(add(d,e))
+		
+	
 main()
