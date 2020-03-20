13b113e80718bfc2252723666748eb83c3671e6e
root <root@localhost.localdomain>
2020-03-20 19:44:17 +0400

    voici un programme de multiplication

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -7,6 +7,9 @@ def mul(a,b):
 def main():
 	import sys
 	entre = sys.argv
+	if (len(entre) != 3):
+		print('Erreur : 2 arguments seulement')		
+		return
 	print(mul(eval(entre[1]),eval(entre[2])))
 main()
 	
