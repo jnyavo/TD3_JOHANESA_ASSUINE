92a18fe91b8e016d2b0b9b65324e7e1d31dd7a24
root <root@localhost.localdomain>
2020-03-20 20:02:54 +0400

    Correction programme

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -7,10 +7,19 @@ def mul(a,b):
 def main():
 	import sys
 	entre = sys.argv
+
 	if (len(entre) != 3):
-		print('Erreur : 2 arguments seulement')		
+		print('Erreur : 2 arguments requis' )
+		a = input('Entrez le premier nombre: ')
+		b = input('Entrez le deuxieme nombre: ')
+		try:
+			print(mul(a,b))
+		except:
+			print(mul(eval(a),eval(b)))		
 		return
+	
 	print(mul(eval(entre[1]),eval(entre[2])))
+
 main()
 	
 
